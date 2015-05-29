efb2b8ce84174dabe8a2324b8e6c6bb42b6a6b1a
Weber Tasai <taida957789@gmail.com>
2015-05-29 20:14:40 +0800

    add Database Connection Class

--- team3/team3/DatabaseConnection.cs
+++ team3/team3/DatabaseConnection.cs
@@ -11,11 +11,29 @@ namespace team3
 {
     class DatabaseConnection
     {
+
         private static List<MySqlConnection> connections = new List<MySqlConnection>();
 
         public static MySqlConnection GetConnection()
         {
+            String connectionStr =  String.Format("server={0};uid={1};pwd={2};database={3}",
+                                        DBSettings.Default.dbHost, 
+                                        DBSettings.Default.dbUser,
+                                        DBSettings.Default.dbPassword,
+                                        DBSettings.Default.dbName);
 
+            MySqlConnection connection = new MySqlConnection(connectionStr);
+
+            connections.Add(connection);
+
+            return connection;
         }
+
+        public static Boolean RemoveConnection(MySqlConnection connection) 
+        {
+            connection.Close();
+            return connections.Remove(connection);
+        }
+        
     }
 }
