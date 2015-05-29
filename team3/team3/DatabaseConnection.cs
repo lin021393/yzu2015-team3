8d73691e11af25979316ef86d8ef20967c6242ec
Weber Tasai <taida957789@gmail.com>
2015-05-29 19:32:25 +0800

    add Sample Setting file

---
+++ team3/team3/DatabaseConnection.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration.Assemblies;
+
+
+namespace team3
+{
+    class DatabaseConnection
+    {
+        private static List<MySqlConnection> connections = new List<MySqlConnection>();
+
+        public static MySqlConnection GetConnection()
+        {
+
+        }
+    }
+}
